d9e96538282dab01d2c4402136626cfcdb64482d
ShipraRaj <114066544+ShipraRaj@users.noreply.github.com>
2022-10-02 23:02:26 +0530

    Add files via upload

---
+++ ThreadLambda.java
@@ -0,0 +1,20 @@
+public class ThreadLambda {
+
+    public static void main(String[] args)
+    {
+        Thread EvenNumberThread = new Thread(() -> {
+            
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    System.out.println("Even Number Thread : "+i);
+                }
+            }
+        });
+        
+        EvenNumberThread.start();
+        
+    }
+
+}
